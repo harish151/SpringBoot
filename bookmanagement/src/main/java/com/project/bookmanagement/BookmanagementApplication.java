6950a7d8095eb6c487cd748531839f47b5560f63
RACHURI HARISH <hr2946352@gmail.com>
2025-03-09 17:26:27 +0530

    Create BookmanagementApplication.java

---
+++ bookmanagement/src/main/java/com/project/bookmanagement/BookmanagementApplication.java
@@ -0,0 +1,18 @@
+package com.project.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagementApplication.class, args);
+	}
+
+}
+
+
+  // requestbody -> controller -> service -> repository
+  //                               | 
+  //                           component
